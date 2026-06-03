36ecf9d62e6a21bfd460e03738cf94ea44594ef1
Nix556 <jonathanjessenjensen@gmail.com>
2026-06-03 14:31:46 +0200

    just the beginning

---
+++ README.md
@@ -0,0 +1,9 @@
+# Neovim Config
+
+My Neovim config.
+
+## Notes
+
+- Still learning Neovim.
+- Not finished.
+- More will be added
---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
