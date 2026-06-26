2bec166997b897c4143b21e80d08f2c20f4d6ea5
Nix556 <jonathanjessenjensen@gmail.com>
2026-06-26 11:02:19 +0200

    new theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("gruvbox")
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
+
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
